bc4947c4ea821d6954dc7c8d97b46aa9def1d259
classicmike <michael.tran.tech@gmail.com>
2015-10-06 19:41:16 +1100

    Continue on iMac

--- js/app.js
+++ js/app.js
@@ -1,30 +1,18 @@
-angular.module('ngMadLibs', ['ngMessages'])
+angular.module('waitStaffCalculator', ['ngMessages'])
     .controller('mainController', function($scope){
 
         $scope.performFormSubmit = function(){
-            if($scope.ngMadLibsForm.$valid){
-                $scope.hideForm = true;
-            }
+
         };
 
         $scope.resetForm = function(){
-            $scope.hideForm = false;
-            $scope.maleName='';
-            $scope.jobTitle='';
-            $scope.tediousTask = '';
-            $scope.dirtyTask = '';
-            $scope.uselessSkill= '';
-            $scope.adjective='';
-            $scope.obnoxiousCelebrity='';
-            $scope.hugeNumber='';
-            $scope.gender = 'Male';
+
         };
 
         $scope.init = function(){
-            $scope.resetForm();
+
         };
 
-        $scope.init();
 
 
     })
