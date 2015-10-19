efd0d4704a8e8c9db9f21010abb033915fb1d089
classicmike <michael.tran.tech@gmail.com>
2015-10-19 21:09:46 +1100

    Continuing exercise

--- js/app.js
+++ js/app.js
@@ -1,61 +1,42 @@
-angular.module('instagramSearcher', ['ngMessages', 'ngAnimate'])
-    .config(function($httpProvider){
-        $httpProvider.defaults.useXDomain = true;
-    })
-    .controller('mainController', function($scope, $sce, $http){
+angular.module('OWMApp', ['ngRoute'])
+    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+        $routeProvider.when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'HomeCtrl'
+        }).when('/cities/:city', {
+            templateUrl: 'views/city.html',
+            controller: 'CityCtrl',
+            resolve: {
+                city: function(owmCities, $route, $location){
+                    var city = $route.current.params.city;
 
-        $scope.initValues = function() {
-            $scope.resultsCount = 0;
-            $scope.searchTerms = '';
-            $scope.enteredTerms = '';
-            $scope.searchResults = [];
-        };
+                    city = city.replace('_' ,' ');
 
-        $scope.trustSrc = function(src){
-            console.log(src);
-            return $sce.trustAsResourceUrl(src);
-        };
+                    if(owmCities.indexOf(city) === -1){
+                        $location.path('/error');
+                        return;
+                    }
 
-        $scope.performSearch = function(){
-            if($scope.instagramSearchForm.$valid){
-                console.log('Need to perform a search');
-                $scope.searchTerms = $scope.enteredTerms;
-                $scope.requestSearchResults();
+                    return city;
+                }
             }
-        };
+        }).when('/error', {
+            template: '<p>Error- Page not found</p>'
+        })
 
-        $scope.requestSearchResults = function(){
-            var apiUrl = $scope.TAG_SEARCH_URL.replace($scope.TAG_NAME_REPLACE, $scope.enteredTerms);
-
-            $http({
-                url: apiUrl,
-                method: 'JSONP'
-            }).then($scope.processResults.bind($scope),
-                function(error){
-                    console.log(error);
-                    alert('Unfortunately there was an error with the function')
-                });
+    }])
+    .value('owmCities', ['New York', 'Dallas', 'Chicago'])
+    .controller('HomeCtrl', function($scope){
+        //insert the controller code here
 
 
-        };
-
-        $scope.processResults = function(results){
-            $scope.searchResults = results.data.data;
-            $scope.resultsCount = $scope.searchResults.length;
-
-        };
-
-        $scope.notifyErrors = function(errors){
-            alert('Unfortunately there was an error in retrieving the images');
-        };
-
-        $scope.TAG_SEARCH_URL = 'https://api.instagram.com/v1/tags/{tag-name}/media/recent?access_token=1579983213.1677ed0.15fcc963b0a0465f9d4c34f6aac0d3a7&callback=JSON_CALLBACK';
-        $scope.TAG_NAME_REPLACE = '{tag-name}';
-
-        $scope.initValues();
+    }).controller('CityCtrl', ['$scope', 'owmCities', function($scope, city){
+        //insert the controller code here
 
 
-    })
+        $scope.city = city;
+
+    }])
     .filter('capsFirst', function(){
         return function(input){
             if(!input){
