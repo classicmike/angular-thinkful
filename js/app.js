2b548861c0362a92671d96286a8e6cc348cf2142
Michael Tran <koramaiku@Michaels-iMac.local>
2015-10-22 12:07:18 +1100

    Completed 1.1.4

--- js/app.js
+++ js/app.js
@@ -16,52 +16,99 @@ angular.module('waitStaffCalculator', ['ngMessages', 'ngRoute'])
         $routeProvider.when('/', {
             templateUrl: 'views/home.html',
             controller: 'HomeController as home'
-        }).when('/new-meal', {
+        }).when('/new-meal/:reset?', {
             templateUrl: 'views/new-meal.html',
             controller: 'NewMealController as newMeal',
             resolve: {
+                reset: function($route){
+                    var reset = $route.current.params.reset;
+                    var resetFlag = false;
 
+                    if(typeof reset !== 'undefined' && reset === 'reset'){
+                        resetFlag = true;
+                    }
+
+                    return resetFlag;
+                }
             }
+        }).when('/my-earnings', {
+            templateUrl: 'views/my-earnings.html',
+            controller: 'MyEarningsController as myEarnings'
         });
     }])
     .controller('HomeController', function(){
         this.welcomeText = 'Hello and welcome to the wait staff calculator. Please enter your figures and the calculator will perform';
         this.title = 'Homepage';
-    }).controller('NewMealController', function(meal, customerCharges, earnings){
+    }).controller('NewMealController', function(meal, customerCharges, earnings, $scope, reset){
         this.resetValues = function(){
-            /*meal.baseMealPrice = 10;
-            meal.tip = 10;
-            meal.tax = 20;
 
-            customerCharges.subTotal = 0.00;
-            customerCharges.tip = 0.00;
-            customerCharges.total = 0.00;
+            var initialMeal = {
+                baseMealPrice: 10,
+                tax: 10,
+                tip: 20
+            };
 
-            earnings.tipTotal = 0.00;
-            earnings.mealCount = 0;
-            earnings.total = 0.00;*/
+            meal = initialMeal;
+
+            var initialCustomerCharges = {
+                subTotal: 0.00,
+                tip: 0.00,
+                total: 0.00
+            };
+
+            customerCharges = initialCustomerCharges;
+
+            var initialEarnings = {
+                tipTotal: 0.00,
+                mealCount: 0,
+                total: 0.00
+            };
+
+            earnings = initialEarnings;
 
         };
 
-        this.initValues = function(){
+        this.initValues = function(toReset){
+            if(toReset){
+                this.resetValues();
+            }
+
             this.meal = meal;
             this.customerCharges = customerCharges;
+
         };
 
         this.performSubmit = function(){
+            if(this.waitStaffCalculatorForm.$valid){
 
+                //calculate the numbers
+                this.customerCharges.subTotal = (1 + this.meal.tax/100) * this.meal.baseMealPrice;
+                this.customerCharges.tip = this.customerCharges.subTotal * (this.meal.tip/100);
+                this.customerCharges.total = this.customerCharges.subTotal + this.customerCharges.tip;
+
+
+                earnings.mealCount++;
+                earnings.tipTotal += this.customerCharges.tip;
+                earnings.total += this.customerCharges.total;
+
+            }
         };
 
+        this.performReset = function(resetFlag){
+            this.initValues(resetFlag);
+        };
 
-        this.initValues();
+        //initialize this controller
+        this.initValues(reset);
 
+    }).controller('MyEarningsController', function(earnings){
+        this.earnings = earnings;
     })
     .filter('capsFirst', function(){
         return function(input){
             if(!input){
                 return input;
             }
-
             return input.charAt(0).toUpperCase() + input.slice(1);
         }
     });
